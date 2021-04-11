6c071f9f3876d52993e942aa820a39aa71724ae2
nbehmane <nbehmane@calpoly.edu>
2021-04-11 11:01:42 -0700

    fixed double import

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,8 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Table from './Table';
 import Form from './Form';
 import axios from 'axios';
-import React, {useState, useEffect} from 'react';
 
 function MyApp() { 
   const [characters, setCharacters] = useState([]);
